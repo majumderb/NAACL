3394f8b5e50bec9d3e163c9254e97164bb6b83d0
Rajesh Bhat <Rajesh.Bhat@walmart.com>
2017-11-11 12:15:33 +0530

    combined morphids of maha & pt files into single data frame

--- sanskrit/naacl_xml_parse.py
+++ sanskrit/naacl_xml_parse.py
@@ -3,24 +3,28 @@ import lxml.etree
 import os
 import pandas as pd
 
-
-xml_files = os.listdir('MBhRenum')
-
-## remove hidden files such as .DS_Store
-final_files= list(set(xml_files).difference(set(['.DS_Store'])))
-
 final_list = []
 
-print len(final_files)
+def prepare_data(folder_name):
 
-for file_name in final_files:
+    xml_files = os.listdir(folder_name)
 
-    with open('MBhRenum/'+file_name, 'r') as f:
-        
-        root = lxml.etree.fromstring(f.read())
-        results = root.findall('pada')
-        morphids = [r.find('morphids').text for r in results]
-        final_list.append([file_name] + [','.join([id for id in morphids if id is not None])])
+    ## remove hidden files such as .DS_Store
+    final_files= list(set(xml_files).difference(set(['.DS_Store'])))
+    
+    print len(final_files)
+
+    for file_name in final_files:
+
+        with open(folder_name+'/'+file_name, 'r') as f:
+            
+            root = lxml.etree.fromstring(f.read())
+            results = root.findall('pada')
+            morphids = [r.find('morphids').text for r in results]
+            final_list.append([file_name] + [','.join([id for id in morphids if id is not None])])
+
+prepare_data('MBhRenum')
+prepare_data('pt1-2000xmlRenum')
 
 df = pd.DataFrame(final_list)
-df.to_csv("maha.csv")
+df.to_csv("combined_maha_pt.csv", index=False)
